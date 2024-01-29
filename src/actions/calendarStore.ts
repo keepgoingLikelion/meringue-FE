90e152cbce416827163d20a64ae6e302ab11e651
yeahzee0421 <ydw08250421@gmail.com>
2024-01-29 13:37:40 +0900

    [Refactor] Code refacoring

--- src/actions/calendarStore.ts
+++ src/actions/calendarStore.ts
@@ -40,15 +40,9 @@ export const useCalendarStore = create<CalendarStore>((set) => {
     const firstDayOfMonth = new Date(date.getFullYear(), date.getMonth(), 1);
     const lastDayOfMonth = new Date(date.getFullYear(), date.getMonth() + 1, 0);
 
-    const prevDayList = Array.from({
-      length: firstDayOfMonth.getDay(),
-    }).map(() => 0);
-
+    const prevDayList = Array(firstDayOfMonth.getDay()).fill(0);
     const currentDayList = Array.from({ length: totalMonthDays }).map((_, i) => i + 1);
-
-    const nextDayList = Array.from({
-      length: 6 - lastDayOfMonth.getDay(),
-    }).map(() => 0);
+    const nextDayList = Array(6 - lastDayOfMonth.getDay()).fill(0);
 
     const weekCalendarList = [...prevDayList, ...currentDayList, ...nextDayList].reduce(
       (acc: number[][], cur, idx) => {
