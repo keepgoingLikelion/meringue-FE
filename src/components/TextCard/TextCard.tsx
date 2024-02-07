f81ad0ebc50e23c1d1c2362f765010e246f57bc6
flareseek <flareseek@gmail.com>
2024-02-07 16:36:26 +0900

    [Feat] #9 - background view

--- src/components/TextCard/TextCard.tsx
+++ src/components/TextCard/TextCard.tsx
@@ -1,6 +1,6 @@
 import styles from './TextCard.module.css';
 
-export default function CardText({ text }: { text: String }) {
+export default function TextCard({ text }: { text: String }) {
   return (
     <div className={styles.CardTextWrapper}>
       <p className={styles.CardText}>{text}</p>
---
+++ src/pages/MainPage/MainPage.tsx
@@ -0,0 +1,11 @@
+import styles from './MainPage.module.css';
+
+export default function MainPage() {
+  return (
+    <>
+      <div className={styles.title} />
+      <div className={styles.wave} />
+      <div>a</div>
+    </>
+  );
+}
