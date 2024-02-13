3a5bd8bc0d9b19703f907e6e2456a43d620cb589
possiblly <so_colorful@naver.com>
2024-02-13 22:31:44 +0900

    [Feat] #14 final
    final

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css';
-import StickerView from './pages/StickerView/StickerView.tsx';
 import TodayPostView from './pages/TodayPostView/TodayPostView.tsx';
 
-function App() {
-  return (
-    <TodayPostView />
-  );
+
+export default function App() {
+ return(
+  <TodayPostView />
+ )
 }
 
-export default App;
+
+
