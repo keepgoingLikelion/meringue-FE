bd57adf3f123941245ddeb5971ae8e5c784a43b2
Jihwan-Hong <124516776+flareseek@users.noreply.github.com>
2024-02-21 16:20:45 +0900

    [Feat] #42 - Add HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
+import { HashRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from 'react-query';
 
 import App from './App.tsx';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter basename="https://keepgoinglikelion.github.io/meringue-FE/">
+      <HashRouter basename="https://keepgoinglikelion.github.io/meringue-FE/">
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </QueryClientProvider>
   </React.StrictMode>,
 );
